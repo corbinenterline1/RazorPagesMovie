63da04b9458750eb268a24e42ba863b7656d3a7d
corbinenterline1 <corbinenterline1@gmail.com>
2022-02-22 20:19:26 -0600

    Initial Creation - Adding Model(s)

---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        // Required by the database for the primary key.
+        public int ID { get; set; }
+        // Title of the movie, default empty string.
+        public string Title { get; set; } = string.Empty;
+        // 
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
